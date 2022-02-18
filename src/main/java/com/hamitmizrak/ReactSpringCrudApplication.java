91abf3291d5d0d414384f7eb9e401b7c032b9504
hamitmizrak <hamitmizrak@gmail.com>
2022-02-18 12:22:57 +0300

    Database olmadan Token oluşturdum

--- src/main/java/com/hamitmizrak/ReactSpringCrudApplication.java
+++ src/main/java/com/hamitmizrak/ReactSpringCrudApplication.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-(exclude =
-        {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
-        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
-        }
-)
+//(exclude =
+//        {
+//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+//        }
+//)
 public class ReactSpringCrudApplication {
 
     public static void main(String[] args) {
